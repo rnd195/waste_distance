6ffea437a0d5ba74963b73e07830612b8ca6c0bd
mranda <mranda@disroot.org>
2020-12-17 23:47:46 +0100

    Ošetření 10km kontejneru a komentáře

--- analyza_kontejneru.py
+++ analyza_kontejneru.py
@@ -22,13 +22,16 @@ def median_slovnik(slovnik):
     return (seznam[pozice] + seznam[pozice + 1]) / 2
 
 
-# DEFINICE PRO PREVOD
+# DEFINICE PRO PREVOD SOURADNIC
 crs_wgs = CRS.from_epsg(4326)  # WGS-84
 crs_jtsk = CRS.from_epsg(5514)  # S-JTSK
+# Konvertovani souradnic z wgs do jtsk formatu
 wgs2jtsk = Transformer.from_crs(crs_wgs, crs_jtsk)
 
 
 # NACITANI DAT
+
+# Pokud soubor chybi nebo je vadny, vypni program
 try:
     with open("stare_mesto_small.geojson", "r", encoding="UTF-8") as file:
         adresy = json.load(file)
@@ -56,19 +59,21 @@ except (FileNotFoundError, NameError, ValueError):
 kont_info = kont["features"]
 
 
-# TODO pomocí xlsx souborů najít divný hodnoty typu missing
+# VYTVARENI SLOVNIKU
 
+# Vytvori se slovnik s unikatnim klicem ulice a cisla popisneho,
+# kde hodnoty daneho slovniku jsou souradnice.
 slovnik_kont = {}
 
 for i in range(len(kont_info)):
-    # osetreni chybejicich klicu
+    # Osetreni chybejicich klicu
     try:
         kont_ulice_cp = kont_info[i]["properties"]["STATIONNAME"]
         kont_souradnice = kont_info[i]["geometry"]["coordinates"]
         kont_pristup = kont_info[i]["properties"]["PRISTUP"]
     except KeyError:
         continue
-
+    # Zapisuje pouze volne pristupne kontejnery
     if kont_pristup == "volně":
         slovnik_kont[kont_ulice_cp] = kont_souradnice
 
@@ -80,13 +85,9 @@ if len(slovnik_kont) == 0:
           "Program se ukonci")
     exit()
 
-
-# vybudovani slovniku adres adresa : souradnice x y
-
 slovnik_adresy = {}
 
 for j in range(len(adresy_info)):
-    # osetreni chybejicich klicu
     try:
         adresa_cp = adresy_info[j]["properties"]["addr:housenumber"]
         adresa_ulice = adresy_info[j]["properties"]["addr:street"]
@@ -96,9 +97,8 @@ for j in range(len(adresy_info)):
         continue
 
     adresa_jtsk = wgs2jtsk.transform(adresa_sirka, adresa_delka)
-    # ulice mezera cislo popisne
-    adresa_ulice_cp = adresa_ulice + " " + adresa_cp
 
+    adresa_ulice_cp = adresa_ulice + " " + adresa_cp
     slovnik_adresy[adresa_ulice_cp] = adresa_jtsk
 
 
@@ -117,7 +117,7 @@ for (klic_adresy, hodnota_adresy) in slovnik_adresy.items():
         x_2 = hodnota_kont[0]
         y_2 = hodnota_kont[1]
 
-        # pythagoras
+        # Pomoci Pythagorovy vety vypocti vzdalenost v metrech (prepona)
         odvesna1 = abs(x_1 - x_2)
         odvesna2 = abs(y_1 - y_2)
         prepona = sqrt((odvesna1 * odvesna1) + (odvesna2 * odvesna2))
@@ -125,6 +125,11 @@ for (klic_adresy, hodnota_adresy) in slovnik_adresy.items():
         docasny_seznam.append(prepona)
 
     min_vzdalenost = min(docasny_seznam)
+    
+    if min_vzdalenost > 10000:
+        print("Kontejner je prilis daleko. Mate spravna data? "
+              "Program se ukonci.")
+        exit()
 
     slovnik_adresy_minkont[klic_adresy] = min_vzdalenost
 
@@ -150,8 +155,8 @@ print(f"Nacteno {len(slovnik_adresy)} adresnich bodu.")
 print(f"Nacteno {len(slovnik_kont)} kontejneru na trideny odpad.")
 
 print("\n"
-      "Prumerna vzdalenost adresniho bodu ke kontejneru je "
+      "Prumerna vzdalenost adresniho bodu k verejnemu kontejneru: "
       f"{prumer_m:.0f} metru.")
-print(f"Median je {median_m:.0f} metru.")
-print(f"Nejdelsi vzdalenost ke kontejnerum je z adresy {max_adresa} "
+print(f"Median vzdalenosti ke kontejneru: {median_m:.0f} metru.")
+print(f"Nejdelsi vzdalenost ke kontejnerum je z adresy '{max_adresa}' "
       f"a to {max_m:.0f} metru.")
