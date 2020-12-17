812388a7393c7d2cd37b47358f271a9075409bce
mranda <mranda@disroot.org>
2020-12-17 17:57:29 +0100

    Pokusy o různé výpočty a orientace v datasetu

--- analyza_kontejneru.py
+++ analyza_kontejneru.py
@@ -1,4 +1,5 @@
 import json
+from math import sqrt
 from pyproj import CRS, Transformer
 
 
@@ -30,3 +31,40 @@ with open("kontejnery_small.json", "r", encoding="UTF-8") as file:
     kont = json.load(file)
 
 kont_info = kont["features"]
+
+
+# jtsk souradnice kontejneru
+print(kont_info[2]["geometry"]["coordinates"])
+# adresa kontejneru
+print(kont_info[2]["properties"]["STATIONNAME"])
+# dostupnost kontejneru
+print(kont_info[2]["properties"]["PRISTUP"])
+
+# ulice
+print(adresy_info[2]["properties"]["addr:street"])
+# cislo popisne
+print(adresy_info[2]["properties"]["addr:housenumber"])
+# kombinace ulice a cp hazi unikatni adresni body
+
+# wgs souradnice adresy
+print(adresy_info[2]["geometry"]["coordinates"])
+
+jtsk_adresy = wgs2jtsk.transform(adresy_info[2]["geometry"]["coordinates"][1],
+                                 adresy_info[2]["geometry"]["coordinates"][0])
+
+print(jtsk_adresy[1])
+
+
+# vzdalenost
+jtsk_kont = kont_info[4]["geometry"]["coordinates"]
+
+odvesna1 = abs(jtsk_kont[0] - jtsk_adresy[0])
+odvesna2 = abs(jtsk_kont[1] - jtsk_adresy[1])
+
+vzdalenost = sqrt((odvesna1 * odvesna1) + (odvesna2 * odvesna2))  # v metrech
+print(vzdalenost)
+
+# TODO přiřadit každé adrese nejbližší kontejner
+# z těchto dvojic zjistit průměr a maximum
+# vyplatí se to házet do dvojic (asi slovník), protože pak budu volat nejdelší
+# vzdálenost
