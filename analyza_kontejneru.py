f77ce232a65e3b6266f931db1534b56bc533b78a
mranda <mranda@disroot.org>
2020-12-17 22:45:50 +0100

    Přeskupení kódu a vyčištění nepotřebných věcí

--- analyza_kontejneru.py
+++ analyza_kontejneru.py
@@ -1,77 +1,52 @@
 import json
 from math import sqrt
-from statistics import mean, median
+# from statistics import mean, median
 from pyproj import CRS, Transformer
 
 
-# definice pro prevod
+def prumer_slovnik(slovnik):
+    prumer = sum(slovnik.values()) / len(slovnik)
+    return prumer
+
+
+def median_slovnik(slovnik):
+    seznam = list(slovnik.values())
+    seznam.sort()
+    n = len(seznam)
+    pozice = (n - 1) // 2
+
+    if n % 2:
+        return seznam[pozice]
+
+    return (seznam[pozice] + seznam[pozice + 1]) / 2
+
+
+# Definice pro prevod
 
 crs_wgs = CRS.from_epsg(4326)  # WGS-84
 crs_jtsk = CRS.from_epsg(5514)  # S-JTSK
-
 wgs2jtsk = Transformer.from_crs(crs_wgs, crs_jtsk)
 
-# https://www.estudanky.eu/prevody/jtsk.php
+# pro kontrolu: https://www.estudanky.eu/prevody/jtsk.php
+# jtsk = wgs2jtsk.transform(50, 15)  # sirka, delka
+# print(jtsk)
 
-jtsk = wgs2jtsk.transform(50, 15)  # sirka, delka
+# jtsk2wgs = Transformer.from_crs(crs_jtsk, crs_wgs)
 
-print(jtsk)
-
-jtsk2wgs = Transformer.from_crs(crs_jtsk, crs_wgs)
-
-print(jtsk2wgs.transform(*jtsk))
+# print(jtsk2wgs.transform(*jtsk))
 
 # loadovani dat
 
-with open("stare_mesto.geojson", "r", encoding="UTF-8") as file:
+with open("stare_mesto_small.geojson", "r", encoding="UTF-8") as file:
     adresy = json.load(file)
 
 adresy_info = adresy["features"]
 
-with open("kontejnery.geojson", "r", encoding="UTF-8") as file:
+with open("kontejnery_small.geojson", "r", encoding="UTF-8") as file:
     kont = json.load(file)
 
 kont_info = kont["features"]
 
-
-# jtsk souradnice kontejneru
-print(kont_info[2]["geometry"]["coordinates"])
-# adresa kontejneru
-print(kont_info[2]["properties"]["STATIONNAME"])
-# dostupnost kontejneru
-print(kont_info[2]["properties"]["PRISTUP"])
-
-# ulice
-print(adresy_info[2]["properties"]["addr:street"])
-# cislo popisne
-print(adresy_info[2]["properties"]["addr:housenumber"])
-# kombinace ulice a cp hazi unikatni adresni body
-
-# wgs souradnice adresy
-print(adresy_info[2]["geometry"]["coordinates"])
-
-jtsk_adresy = wgs2jtsk.transform(adresy_info[2]["geometry"]["coordinates"][1],
-                                 adresy_info[2]["geometry"]["coordinates"][0])
-
-print(jtsk_adresy[1])
-
-
-# vzdalenost
-jtsk_kont = kont_info[4]["geometry"]["coordinates"]
-
-odvesna1 = abs(jtsk_kont[0] - jtsk_adresy[0])
-odvesna2 = abs(jtsk_kont[1] - jtsk_adresy[1])
-
-vzdalenost = sqrt((odvesna1 * odvesna1) + (odvesna2 * odvesna2))  # v metrech
-print(vzdalenost)
-
-# TODO přiřadit každé adrese nejbližší kontejner
-# z těchto dvojic zjistit průměr a maximum
-# vyplatí se to házet do dvojic (asi slovník), protože pak budu volat nejdelší
-# vzdálenost
-
-
-# vybudovani slovniku kontejneru adresa : souradnice x y
 
 # TODO přidat nějaký check, kdyby to nebyl float (pak skip)
 # TODO pomocí xlsx souborů najít divný hodnoty typu missing
@@ -116,11 +91,11 @@ for j in range(len(adresy_info)):
     # ulice mezera cislo popisne
     adresa_ulice_cp = adresa_ulice + " " + adresa_cp
 
-    # budovani slovniku
     slovnik_adresy[adresa_ulice_cp] = adresa_jtsk
 
 
 # hledani nejmensi vzdalenosti pro danou adresu
+# TODO kontejner nad 10 000 m
 
 slovnik_adresy_minkont = {}
 
@@ -146,33 +121,22 @@ for (klic_adresy, hodnota_adresy) in slovnik_adresy.items():
     slovnik_adresy_minkont[klic_adresy] = min_vzdalenost
 
 
-# TODO def prumer
-mean(slovnik_adresy_minkont.values())
-prumer_m = sum(slovnik_adresy_minkont.values()) / len(slovnik_adresy_minkont)
+# PRUMER, MEDIAN A MAXIMUM
 
-# TODO premistit nahoru
+# print(mean(slovnik_adresy_minkont.values()))
+prumer_m = prumer_slovnik(slovnik_adresy_minkont)
 
-
-def median_slovnik(slovnik):
-    seznam = list(slovnik.values())
-    seznam.sort()
-    n = len(seznam)
-    pozice = (n - 1) // 2
-
-    if n % 2:
-        return seznam[pozice]
-
-    return (seznam[pozice] + seznam[pozice + 1]) / 2
-
-
+# print(median(slovnik_adresy_minkont.values()))
 median_m = median_slovnik(slovnik_adresy_minkont)
-median_kontrola = median(slovnik_adresy_minkont.values())
 
 max_m = max(slovnik_adresy_minkont.values())
 
 for (klic, hodnota) in slovnik_adresy_minkont.items():
     if hodnota == max_m:
         max_adresa = klic
+
+
+# FINALNI SOUHRN
 
 print(f"Nacteno {len(slovnik_adresy)} adresnich bodu.")
 print(f"Nacteno {len(slovnik_kont)} kontejneru na trideny odpad.")
