0527af7cc767fc401e6a2ad0f82f37d8afaff322
mranda <mranda@disroot.org>
2020-12-29 20:47:40 +0100

    Předělání hlavních bloků kódu do funkcí a kosmetické změny

--- analyza_kontejneru.py
+++ analyza_kontejneru.py
@@ -40,6 +40,79 @@ def nacitani_geojson(jmeno_souboru):
     return soubor
 
 
+def slovnik_verejne_kont(konejnery_info):
+    """Vytvori slovnik s unikatnim klicem ulice a cisla popisneho, kde hodnoty
+    daneho slovniku jsou souradnice. V pripade duplicitnich adres se z
+    podstaty slovniku bere v potaz jen 1 adresa. Proto se muze pocet prvku
+    vlozeneho slovniku lisit od poctu prvku ve finalnim (novem) slovniku."""
+
+    slovnik_kontejnery = {}
+    # Pocitadlo chybejicich klicu - chybi-li klic, pricte 1 a pozdeji upozorni
+    vyrazene_kont = 0
+
+    for kontejner in konejnery_info:
+        try:
+            kont_ulice_cp = kontejner["properties"]["STATIONNAME"]
+            kont_souradnice = kontejner["geometry"]["coordinates"]
+            kont_pristup = kontejner["properties"]["PRISTUP"]
+        except KeyError:
+            vyrazene_kont += 1
+            continue
+
+        # Zapisuje do slovniku pouze volne pristupne kontejnery
+        if kont_pristup == "volně":
+            slovnik_kontejnery[kont_ulice_cp] = kont_souradnice
+
+    if vyrazene_kont > 0:
+        print(
+            f"Upozorneni: bylo vyrazeno {vyrazene_kont} kontejneru kvuli "
+            "chybejicimu klici."
+        )
+
+    # Pokud nejsou v souboru zadne volne kontejnery, vypni program.
+    if len(slovnik_kontejnery) == 0:
+        print(
+            "V souboru s kontejnery neni zadny volne pristupny. "
+            "Program skonci."
+        )
+        exit()
+
+    return slovnik_kontejnery
+
+
+def slovnik_adresnich_bodu(adresni_body_info):
+    """Vytvori slovnik adresnich bodu s prevedenymi jtsk souradnicemi.
+    Principielne funguje stejne jako funkce slovnik_verejne_kont."""
+
+    slovnik_adresy = {}
+    vyrazene_body = 0
+
+    for bod in adresni_body_info:
+        try:
+            adresa_cp = bod["properties"]["addr:housenumber"]
+            adresa_ulice = bod["properties"]["addr:street"]
+            # Souradnice jsou prehozene v souboru s adresami
+            adresa_sirka = bod["geometry"]["coordinates"][1]
+            adresa_delka = bod["geometry"]["coordinates"][0]
+        except KeyError:
+            vyrazene_body += 1
+            continue
+
+        # Prevod wgs souradnic na jtsk format
+        adresa_jtsk = wgs2jtsk.transform(adresa_sirka, adresa_delka)
+
+        adresa_ulice_cp = adresa_ulice + " " + adresa_cp
+        slovnik_adresy[adresa_ulice_cp] = adresa_jtsk
+
+    if vyrazene_body > 0:
+        print(
+            f"Upozorneni: bylo vyrazeno {vyrazene_body} adresnich bodu kvuli "
+            "chybejicimu klici."
+        )
+
+    return slovnik_adresy
+
+
 def vzdalenost_pythagoras(bod1_x, bod2_x, bod1_y, bod2_y):
     """Vypocita vzdalenost dvou bodu podle Pythagorovy vety."""
     odvesna1 = abs(bod1_x - bod2_x)
@@ -48,6 +121,46 @@ def vzdalenost_pythagoras(bod1_x, bod2_x, bod1_y, bod2_y):
     prepona = sqrt((odvesna1 * odvesna1) + (odvesna2 * odvesna2))
 
     return prepona
+
+
+def min_vzdal_adresa_kont(slovnik_kontejnery, slovnik_adresy):
+    """Z vytvorenych slovniku adres a kontejneru za pomoci funkce
+    vzdalenost_pythagoras vygeneruje slovnik s adresnimi body a vzdalenosti
+    k nejblizsimu kontejneru."""
+
+    slovnik_adresy_minkont = {}
+
+    for (adresa_bod, souradnice_bod) in slovnik_adresy.items():
+        # Souradnice adresniho bodu
+        x_1 = souradnice_bod[0]
+        y_1 = souradnice_bod[1]
+
+        # Dummy minimalni hodnota na porovnavani vzdalenosti v cyklu nize
+        min_vzdalenost = 999999
+
+        for souradnice_kont in slovnik_kontejnery.values():
+            # Souradnice kontejneru
+            x_2 = souradnice_kont[0]
+            y_2 = souradnice_kont[1]
+
+            vzdalenost = vzdalenost_pythagoras(x_1, x_2, y_1, y_2)
+
+            # Porovnava aktualni hodnoty proti nejnizsi (zatim) nalezene
+            if vzdalenost < min_vzdalenost:
+                min_vzdalenost = vzdalenost
+
+        # Osetreni 10+km vzdaleneho kontejneru
+        if min_vzdalenost > 10000:
+            print(
+                "Nektery z kontejneru je vzdalen vice nez 10 km od "
+                "nejblizsiho adresniho bodu.\n"
+                "Mate spravna data? Program se ukonci."
+            )
+            exit()
+
+        slovnik_adresy_minkont[adresa_bod] = min_vzdalenost
+
+    return slovnik_adresy_minkont
 
 
 def prumer_slovnik(slovnik):
@@ -83,113 +196,20 @@ wgs2jtsk = Transformer.from_crs(crs_wgs, crs_jtsk)
 # Nastavi pracovni adresar na filepath
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-adresy_info = nacitani_geojson("adresy.geojson")["features"]
 kont_info = nacitani_geojson("kontejnery.geojson")["features"]
+adresy_info = nacitani_geojson("adresy.geojson")["features"]
 
 
 # SLOVNIK VEREJNYCH KONTEJNERU
-
-# Vytvori se slovnik s unikatnim klicem ulice a cisla popisneho, kde hodnoty
-# daneho slovniku jsou souradnice. V pripade duplicitnich adres se z podstaty
-# slovniku bere v potaz jen 1 adresa. Proto se muze pocet prvku v
-# adresy_info/kont_info lisit od poctu prvku v slov_kont/slov_adresy.
-
-slov_kont = {}
-# Pocitadlo chybejicich klicu - chybi-li klic, pricte 1 a pozdeji upozorni
-vyrazene_kont = 0
-
-for kontejner in kont_info:
-    try:
-        kont_ulice_cp = kontejner["properties"]["STATIONNAME"]
-        kont_souradnice = kontejner["geometry"]["coordinates"]
-        kont_pristup = kontejner["properties"]["PRISTUP"]
-    except KeyError:
-        vyrazene_kont += 1
-        continue
-
-    # Zapisuje do slovniku pouze volne pristupne kontejnery
-    if kont_pristup == "volně":
-        slov_kont[kont_ulice_cp] = kont_souradnice
-
-if vyrazene_kont > 0:
-    print(
-        f"Upozorneni: bylo vyrazeno {vyrazene_kont} kontejneru kvuli "
-        "chybejicimu klici."
-    )
-
-
-# Pokud nejsou v souboru zadne volne kontejnery, vypni program.
-if len(slov_kont) == 0:
-    print(
-        "V souboru s kontejnery neni zadny volne pristupny. Program skonci."
-    )
-    exit()
+slov_kont = slovnik_verejne_kont(kont_info)
 
 
 # SLOVNIK ADRESNICH BODU
-
-slov_adresy = {}
-vyrazene_body = 0
-
-# Vytvori slovnik adresnich bodu s prevedenymi jtsk souradnicemi
-for bod in adresy_info:
-    try:
-        adresa_cp = bod["properties"]["addr:housenumber"]
-        adresa_ulice = bod["properties"]["addr:street"]
-        # Souradnice jsou prehozene v souboru s adresami
-        adresa_sirka = bod["geometry"]["coordinates"][1]
-        adresa_delka = bod["geometry"]["coordinates"][0]
-    except KeyError:
-        vyrazene_body += 1
-        continue
-
-    # Prevod wgs souradnic na jtsk format
-    adresa_jtsk = wgs2jtsk.transform(adresa_sirka, adresa_delka)
-
-    adresa_ulice_cp = adresa_ulice + " " + adresa_cp
-    slov_adresy[adresa_ulice_cp] = adresa_jtsk
-
-if vyrazene_body > 0:
-    print(
-        f"Upozorneni: bylo vyrazeno {vyrazene_body} adresnich bodu kvuli "
-        "chybejicimu klici."
-    )
+slov_adresy = slovnik_adresnich_bodu(adresy_info)
 
 
 # HLEDANI NEJMENSI VZDALENOSTI PRO DANOU ADRESU
-
-# Slovnik, kde klicem je adresa a hodnotou vzdalenost nejblizsiho kontejneru
-slov_adresy_minkont = {}
-
-for (adresa_bod, souradnice_bod) in slov_adresy.items():
-    # Souradnice adresniho bodu
-    x_1 = souradnice_bod[0]
-    y_1 = souradnice_bod[1]
-
-    # Dummy minimalni hodnota na porovnavani vzdalenosti v nasledujicim cyklu
-    min_vzdalenost = 999999
-
-    for souradnice_kont in slov_kont.values():
-        # Souradnice kontejneru
-        x_2 = souradnice_kont[0]
-        y_2 = souradnice_kont[1]
-
-        vzdalenost = vzdalenost_pythagoras(x_1, x_2, y_1, y_2)
-
-        # Porovnava aktualni hodnoty proti nejnizsi (zatim) nalezene
-        if vzdalenost < min_vzdalenost:
-            min_vzdalenost = vzdalenost
-
-    # Osetreni 10+km vzdaleneho kontejneru
-    if min_vzdalenost > 10000:
-        print(
-            "Nektery z kontejneru je vzdalen vice nez 10 km od nejblizsiho "
-            "adresniho bodu.\n"
-            "Mate spravna data? Program se ukonci."
-        )
-        exit()
-
-    slov_adresy_minkont[adresa_bod] = min_vzdalenost
+slov_adresy_minkont = min_vzdal_adresa_kont(slov_kont, slov_adresy)
 
 
 # PRUMER, MEDIAN A MAXIMUM
@@ -202,9 +222,9 @@ median_m = median_slovnik(slov_adresy_minkont)
 
 max_m = max(slov_adresy_minkont.values())
 
-# K maximu priradit odpovidajici adresu
-for (adresa, vzdalenost) in slov_adresy_minkont.items():
-    if vzdalenost == max_m:
+# K maximu vzdalenosti priradit odpovidajici adresu
+for (adresa, vzdal) in slov_adresy_minkont.items():
+    if vzdal == max_m:
         max_adresa = adresa
 
 
